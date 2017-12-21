7e330cce3e0edccfba2151b0a4d38f890bc32dff
Tom Henderson <tomhenderson@mac.com>
2017-12-22 08:42:24 +1300

    Allow setup.py to be run from any path

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+import os
 from distutils.core import setup
 from setuptools import find_packages
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-common',
