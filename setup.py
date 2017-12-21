081cdcee6c2937e48be4b18fa7899adb341445cc
Tom Henderson <tomhenderson@mac.com>
2017-12-22 08:40:09 +1300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='django-common',
     version='0.0.1',
     author=u'Tom Henderson',
     author_email='tomhenderson@mac.com',
-    packages=['django_common'],
+    packages=find_packages(),
     url='https://github.com/tom-henderson/django-common',
     license='MIT licence',
     description='Reusable views, mixins and templates for Django',
     long_description=(open('README.md').read() + '\n\n' +
                       open('CHANGELOG.md').read()),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires = [
+        'django>=1.7',
+        'markdown>=2.6',
+    ]
 )
